564e78fa977f44c8fc4e4920a7ebefef0c5f6c3f
Matheus Soares <124801798+MatheusRSoarez@users.noreply.github.com>
2024-05-06 12:45:16 -0300

    Add files via upload

---
+++ ex2/ex2/RegistroDeCompra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class RegistroDeCompra
+    {
+        public DateTime Data;
+        public string Produto;
+        public int Valor;
+
+
+    }
+}
