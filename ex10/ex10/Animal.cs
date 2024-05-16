6a9f2272878650bc207bde429cb25ae7e81a4bd2
Matheus Soares <124801798+MatheusRSoarez@users.noreply.github.com>
2024-05-16 20:39:34 -0300

    Add files via upload

---
+++ ex10/ex10/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    public class Animal
+    {
+        // Propriedades da classe Animal
+        public string Nome { get; set; }
+        public string Espécie { get; set; }
+        public int Idade { get; set; }
+
+        // Construtor da classe Animal
+        public Animal(string nome, string espécie, int idade)
+        {
+            Nome = nome;
+            Espécie = espécie;
+            Idade = idade;
+        }
+
+        // Método para emitir o som do animal
+        public void EmitirSom()
+        {
+            if (Espécie.ToLower() == "cachorro")
+            {
+                Console.WriteLine("Au au!");
+            }
+            else if (Espécie.ToLower() == "gato")
+            {
+                Console.WriteLine("Miau!");
+            }
+            else
+            {
+                Console.WriteLine("Som do animal desconhecido.");
+            }
+        }
+    }
+
+}
---
+++ ex10/ex10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso da classe Animal
+            Animal cachorro = new Animal("Rex", "Cachorro", 5);
+            cachorro.EmitirSom();  // Imprime "Au au!"
+
+            Animal gato = new Animal("Whiskers", "Gato", 3);
+            gato.EmitirSom();  // Imprime "Miau!"
+
+            Animal papagaio = new Animal("Loro", "Papagaio", 10);
+            papagaio.EmitirSom();  // Imprime "Som do animal desconhecido."
+        }
+    }
+}
